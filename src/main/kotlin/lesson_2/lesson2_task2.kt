c0bf619e42c898304ea95cfcc5b97e3f73f8dbff
Dmitry <mistapecss@yandex.ru>
2024-09-14 15:45:38 +0300

    KS-2-1 decision task2

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main(){
+    var sumWorkers:Int = 50
+    var sumInterns:Int = 30
+    var selaryWorkers:Int = 50000
+    var selaryInterns:Int = 30000
+    var sumSelaryWorkers:Int = sumWorkers * selaryWorkers
+    var sumAllSelayes:Int = sumSelaryWorkers + sumInterns * selaryInterns
+    var middleSelaryes:Int = sumAllSelayes / (sumWorkers + sumInterns)
+    println("$sumSelaryWorkers\n$sumAllSelayes\n$middleSelaryes")
+}
