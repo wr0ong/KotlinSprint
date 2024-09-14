c38b323edd5967215842a7ef9b7767e416923ebc
Dmitry <mistapecss@yandex.ru>
2024-09-14 16:05:51 +0300

    KS-2-1 decision task4

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main(){
+    var crystalMineral:Int = 11
+    var irenMineral:Int = 7
+    val buffPower: Float = 0.2f
+    var crystalMineralFromBuff:Float = crystalMineral*buffPower
+    var irenMineralFromBuff:Float = irenMineral*buffPower
+    println("${crystalMineralFromBuff.toInt()}\n${irenMineralFromBuff.toInt()}")
+}
