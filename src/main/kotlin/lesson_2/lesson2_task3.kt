380c6645d94093c20652d7130edf3f81381b380c
Dmitry <mistapecss@yandex.ru>
2024-09-14 15:55:48 +0300

    KS-2-1 decision task3

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main() {
+    var hourOfTrainStart:Int = 9
+    var minuteOfTrainStart:Int = 39
+    var minutesInWay:Int = 457
+    var hourOfTrainFinish:Int = (minutesInWay+39)/60+9
+    var minuteOfTrainFinish:Int = minutesInWay%60
+    println("$hourOfTrainFinish:$minuteOfTrainFinish")
+}
