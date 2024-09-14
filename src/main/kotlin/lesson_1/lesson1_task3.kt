3e0ec9b04c919f8eb030c17d0316d8cbfb8604f9
Dmitry <mistapecss@yandex.ru>
2024-09-14 12:23:00 +0300

    KS-1-3 decision task3

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,2 +1,11 @@
 package org.example.lesson_1
 
+fun main(){
+    val year:Int = 1961
+    var hour:String = "09"
+    var minute:String = "07"
+    println("$year\n$hour\n$minute")
+    hour = "10"
+    minute = "55"
+    println ("$hour:$minute")
+}
